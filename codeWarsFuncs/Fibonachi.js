2e2ac92d30e025c0c3181fd7fa08ee350f70ca2a
cisco-laptop <feelgeel@gmail.com>
2023-02-26 17:25:09 +0100

    tribunocio

--- codeWarsFuncs/Fibonachi.js
+++ codeWarsFuncs/Fibonachi.js
@@ -1,4 +1,14 @@
-function fabinouchi(params) {
-    console.log("hello")
+function tribonacci(signature,n){
+    if(n===0)return []
+    let newArr=[...signature]
+    n=n+3
+    for (let i = 0; i < n; i++) {
+        let position=newArr.length-1
+    let sumArr=newArr[position]+newArr[position-1]+newArr[position-2]
+    newArr.push(sumArr)
+    console.log(sumArr)
 }
- fabinouchi()
+console.log(newArr)
+// return newArr
+  }
+ tribonacci([1,1,1],10)
